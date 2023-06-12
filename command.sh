98efeea518d6fc32c60f3e7bb42dbfa5f1b678f2
Ben <bvt3@ymail.com>
2023-06-12 20:18:55 +0800

    Change on command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,2 @@
-az webapp up --name benprj2 --resource-group UdacityProject2 --sku F1
+az webapp up --name benprj2 --resource-group UdacityProject2 --sku B1 --runtime "PYTHON:3.9"
 echo https://benprj2.azurewebsites.net
