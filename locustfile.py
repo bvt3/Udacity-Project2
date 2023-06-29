238289b6a04980a70a5adc8c79f330cedfc548da
Ben <bvt3@ymail.com>
2023-06-29 11:59:23 +0800

    Change app name

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser,task,between
 
 class MyUser(HttpUser):
     wait_time=between(1, 5)
-    host="https://benprj2app.azurewebsites.net"
+    host="https://benprj2.azurewebsites.net"
 
     @task
     def currTest(self):
